1d1ac8ed9dceb7cc8b1f8c8a6a95c355931a20b3
alexitaylor <alexi@bluesunrise.com>
2017-05-08 09:48:11 -0700

    Removing redux Pt II

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -16,16 +14,10 @@ import PhotoGrid from './components/photogrid/PhotoGrid';
 import Photo from './components/photo/Photo';
 import Profile from './components/profile/Profile';
 
-import reducers from './reducers';
-
 import './components/bundle.scss';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
   <MuiThemeProvider>
-  <Provider store={store}>
     <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid} />;
@@ -33,6 +25,5 @@ ReactDOM.render(
         <Route path="/profile" component={Profile} />
       </Route>
     </Router>
-  </Provider>
   </MuiThemeProvider>
   , document.getElementById('react-root'));
