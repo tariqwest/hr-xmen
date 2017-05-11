690eff5a0ec1d5345d7724b5154a1a03fff4396d
alexitaylor <alexi@bluesunrise.com>
2017-05-10 20:15:14 -0700

    Full implementation of Flux
    Now able to manage state with route transition between componenets.

--- client/app/components/photoinfo/PhotoInfo.jsx
+++ client/app/components/photoinfo/PhotoInfo.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import photoStore from '../../stores/photoStore';
 import photoActions from '../../actions/photoActions';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+
 class PhotoInfo extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      photoList: photoStore.getList(),
-      currentPhoto: photoStore.getCurrent()
+      list: photoStore.getList(),
+      current: photoStore.getCurrent()
     }
   }
 
@@ -16,7 +19,18 @@ class PhotoInfo extends React.Component {
     return (
       <div className="photoinfo">
         <h1>PhotoInfo</h1>
-        <p>something: {this.state.currentPhoto.img}</p>
+        <Card>
+          <CardHeader title="URL Avatar" subtitle="Subtitle" avatar="http://www.hdwallpapers.in/walls/neytiri_in_avatar_2-wide.jpg" />
+          <CardMedia overlay={<CardTitle title={this.state.current.title} />} >
+          <img src={this.state.current.img} className="foodImg"/>
+          </CardMedia>
+          <CardTitle title={this.state.current.title} />
+          <CardText>
+          {this.state.current.description}
+          </CardText>
+          <CardActions>
+          </CardActions>
+        </Card>
       </div>
     )
   }
