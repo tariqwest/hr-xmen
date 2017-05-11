a8fe9053196df35f244c351d4c6ecac7b5b7e444
alexitaylor <alexi@bluesunrise.com>
2017-05-11 15:09:29 -0700

    Added paper style to card

--- client/app/components/photo/Photo.jsx
+++ client/app/components/photo/Photo.jsx
@@ -8,6 +8,7 @@ import defaultData from '../../default.js'
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
 
 class Photo extends React.Component {
   constructor(props) {
@@ -22,21 +23,23 @@ class Photo extends React.Component {
     return (
       <div className="photo">
         <h1>Photo</h1>
-        <Card>
-          <CardHeader title="URL Avatar" subtitle="Subtitle" avatar="http://www.hdwallpapers.in/walls/neytiri_in_avatar_2-wide.jpg" />
-          <CardMedia overlay={<CardTitle title={this.state.current.title || defaultData.currentDefault.title} />} >
-          <img src={this.state.current.img || defaultData.currentDefault.img} className="foodImg"/>
-          </CardMedia>
-          <CardTitle title={this.state.current.title || defaultData.currentDefault.title} />
-          <CardText>
-          {this.state.current.description || defaultData.currentDefault.description}
-          </CardText>
-          <CardActions>
-            <Link to="/photos/photoinfo">
-              <RaisedButton label="GET MORE INFO" primary={true} fullWidth={true}/>
-            </Link>
-          </CardActions>
-        </Card>
+        <Paper zDepth={4} rounded={true}>
+          <Card>
+            <CardHeader title="URL Avatar" subtitle="Subtitle" avatar="http://www.hdwallpapers.in/walls/neytiri_in_avatar_2-wide.jpg" />
+            <CardMedia overlay={<CardTitle title={this.state.current.title || defaultData.currentDefault.title} />} >
+            <img src={this.state.current.img || defaultData.currentDefault.img}/>
+            </CardMedia>
+            <CardTitle title={this.state.current.title || defaultData.currentDefault.title} />
+            <CardText>
+            {this.state.current.description || defaultData.currentDefault.description}
+            </CardText>
+            <CardActions>
+              <Link to="/photos/photoinfo">
+                <RaisedButton label="GET MORE INFO" primary={true} fullWidth={true}/>
+              </Link>
+            </CardActions>
+          </Card>
+        </Paper>
       </div>
     )
   }
