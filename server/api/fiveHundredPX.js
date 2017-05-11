d9f51ae49429a0df28b5806e5b412c0ae3a74f23
alexitaylor <alexi@bluesunrise.com>
2017-05-11 13:22:33 -0700

    Able to send 500px API data to client

---
+++ server/api/fiveHundredPX.js
@@ -0,0 +1,21 @@
+var FiveHundredPX = require('500px');
+var api500px = new FiveHundredPX(process.env.FIVEHUNDREDPX_KEY);
+
+
+module.exports = {
+    searchPhotos: (food, res) => {
+        api500px.photos.searchByTerm( food , {'sort': '_score', 'rpp': '100', 'image_size': 440, 'only': 23}, (error, results) => {
+        if (error) {
+            console.log(error);
+        }
+        var tilesData = results.photos.map((item) => {
+            return {
+                img: item.image_url,
+                title: item.name,
+                description: item.description
+            }
+        })
+        res.json(tilesData)
+         })
+    }
+}
