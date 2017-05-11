1ddebf0f0d4aba4bb08237bc7cd876311ccdcd73
alexitaylor <alexi@bluesunrise.com>
2017-05-11 13:25:50 -0700

    Implemented default data for components

--- client/app/components/photo/Photo.jsx
+++ client/app/components/photo/Photo.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 
 import photoStore from '../../stores/photoStore';
 import photoActions from '../../actions/photoActions';
+import defaultData from '../../default.js'
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -23,12 +24,12 @@ class Photo extends React.Component {
         <h1>Photo</h1>
         <Card>
           <CardHeader title="URL Avatar" subtitle="Subtitle" avatar="http://www.hdwallpapers.in/walls/neytiri_in_avatar_2-wide.jpg" />
-          <CardMedia overlay={<CardTitle title={this.state.current.title} />} >
-          <img src={this.state.current.img} className="foodImg"/>
+          <CardMedia overlay={<CardTitle title={this.state.current.title || defaultData.currentDefault.title} />} >
+          <img src={this.state.current.img || defaultData.currentDefault.img} className="foodImg"/>
           </CardMedia>
-          <CardTitle title={this.state.current.title} />
+          <CardTitle title={this.state.current.title || defaultData.currentDefault.title} />
           <CardText>
-          {this.state.current.description}
+          {this.state.current.description || defaultData.currentDefault.description}
           </CardText>
           <CardActions>
             <Link to="/photos/photoinfo">
