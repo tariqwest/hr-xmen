6039c1c5c1849b63c4c28ab4e7519340a18ce4b0
alexitaylor <alexi@bluesunrise.com>
2017-05-14 16:29:24 -0700

    Fix openMenu API, photo search is now working

--- client/app/components/photogrid/PhotoGrid.jsx
+++ client/app/components/photogrid/PhotoGrid.jsx
@@ -44,7 +44,6 @@ class PhotoGrid extends React.Component {
       }
 
       geolocation.getCurrentPosition((position) => {
-        console.log('position', position);
         photoActions.addLocation(position.coords);
       }, () => {
         reject(new Error('Permission denied'));
